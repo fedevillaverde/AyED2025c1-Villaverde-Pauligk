148956cf9eac830ba60b3621797e242ef814f3bc
CrisMoreiraYanz <cmoreirayanz@gmail.com>
2025-03-28 13:51:38 -0300

    ordenamiento burbuja

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,12 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
+def ordenamientoburbuja(lista):
+    for num_lista in range(len(lista)-1,0,-1):
+        for i in range(num_lista):
+            if lista[i] > lista[i+1]:
+                temp=lista[i]
+                lista[i]=lista[i+1]
+                lista[i+1]=temp
+    return lista
+listita=[3,5,8,4,18,9,13,1,56,97,77,68,21,11]
+una_lista=ordenamientoburbuja(listita)
+print(una_lista)
+                
