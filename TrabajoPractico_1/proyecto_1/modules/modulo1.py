e2450693ee469a07e585c5c39418ccd0b9a7a0c4
unknown <federicovillaverde00@gmail.com>
2025-04-02 16:26:20 -0300

    Función creadora de listas

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,12 +1,26 @@
+import random as r
+
+def randint_list(cantidad):
+    listarda =[]
+    for i in range(cantidad):
+        valor= r.randint(0,10000000)
+        listarda.append(valor)
+    return listarda
+
 def ordenamientoburbuja(lista):
+    #Realiza tantas pasadas como pares de items tenga la lista
     for num_lista in range(len(lista)-1,0,-1):
+
+        #En cada pasada, realiza una iteración menos, ya que deja los valores más grandes al final
         for i in range(num_lista):
             if lista[i] > lista[i+1]:
                 temp=lista[i]
                 lista[i]=lista[i+1]
                 lista[i+1]=temp
+
     return lista
-listita=[3,5,8,4,18,9,13,1,56,97,77,68,21,11]
+
+listita= randint_list(500)
 una_lista=ordenamientoburbuja(listita)
 print(una_lista)
                 
