93f6d9d5a6190e5ebbfc3df3bc37d3dca8d47d64
Pauligk <pauligkmaximo@gmail.com>
2025-05-16 14:06:15 -0300

    Avance de ejercicio 2

--- TrabajoPractico_2/Ejercicio 2/modules/ArbolAVL.py
+++ TrabajoPractico_2/Ejercicio 2/modules/ArbolAVL.py
@@ -1,3 +1,4 @@
+import NodoArbol
 class ArbolAvl:
     def __init__(self):
         self.__raiz=None
@@ -10,6 +11,27 @@ class ArbolAvl:
         return self.__tamano
     
     def __iter__(self):
-        return self.__raiz.__iter__()
+        return self.__raiz
+    def agregar(self,nuevita_clavecita,nuevito_valorcito):
+        if self.__raiz:
+            self._agregar(nuevita_clavecita,nuevito_valorcito,self.__raiz)
+        else:
+            self.__raiz = NodoArbol(nuevita_clavecita,nuevito_valorcito)
+    def _agregar(self, nuevita_clavecita,nuevito_valorcito,nodoActual):
+        if nuevita_clavecita < self.__nodoActual.clave:
+            if nodoActual.tieneHijoIzquierdo():
+                self._agregar(nuevita_clavecita,nuevito_valorcito,nodoActual.hijoIzquierdo)
+            else:
+                nodoActual.hijoIzquierdo = NodoArbol(nuevita_clavecita,nuevito_valorcito,padre=nodoActual)
+                self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
+        else:
+            if nodoActual.tieneHijoDerecho():
+                self._agregar(nuevita_clavecita,nuevito_valorcito,nodoActual.hijoDerecho)
+            else:
+                nodoActual.hijoDerecho = NodoArbol(nuevita_clavecita,nuevito_valorcito,padre = nodoActual)
+                self.actualizarEquilibrio(nodoActual.hijoDerecho)
     
+            
+            
+        
     
--- TrabajoPractico_2/Ejercicio 2/modules/NodoArbol.py
+++ TrabajoPractico_2/Ejercicio 2/modules/NodoArbol.py
@@ -5,6 +5,7 @@ class NodoArbol:
         self.__hijoIzquierdo=izquierdo
         self.__hijoDerecho=derecho
         self.__padre=padre
+        self.__factorEquilibrio = 0
     @property
     def clave(self):
         return self.__clave
@@ -20,6 +21,12 @@ class NodoArbol:
     @property
     def padre(self):
         return self.__padre
+    @property
+    def factorEquilibrio(self):
+        return self.__factorEquilibrio
+    @factorEquilibrio.setter
+    def factorEquilibrio(self,nuevito_valorcito):
+        self.__factorEquilibrio = nuevito_valorcito
     @hijoIzquierdo.setter
     def hijoIzquierdo(self,hijoIzquierdo):
         self.__hijoIzquierdo=hijoIzquierdo
@@ -29,4 +36,28 @@ class NodoArbol:
     @padre.setter
     def padre(self,padre):
         self.__padre=padre
-    
+    def tieneHijoIzquierdo(self):
+        return self.__hijoIzquierdo
+    def tieneHijoDerecho(self):
+       return self.__hijoDerecho
+    def esHijoIzquierdo(self):
+        return self.__padre and self.__padre.hijoIzquierdo == self
+    def esHijoDerecho(self):
+        return self.__padre and self.__padre.hijoDerecho == self
+    def esRaiz(self):
+        return not self.__padre
+    def esHoja(self):
+        return not (self.__hijoDerecho or self.__hijoIzquierdo)
+    def tieneAlgunHijo(self):
+        return self.__hijoDerecho or self.__hijoIzquierdo
+    def tieneAmbosHijos(self):
+        return self.__hijoDerecho and self.__hijoIzquierdo
+    def reemplazarDatoDeNodo(self,clave,valor,hizq,hder):
+        self.__clave=clave
+        self.__cargaUtil=valor
+        self.__hijoIzquierdo=hizq
+        self.__hijoDerecho=hder
+        if self.tieneHijoIzquierdo():
+            self.__hijoIzquierdo=self
+        if self.tieneHijoDerecho():
+            self.__hijoIzquierdo = self
